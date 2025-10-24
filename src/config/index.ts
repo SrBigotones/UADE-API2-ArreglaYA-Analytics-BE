9b187ebb036720a63d5480f8689cf0485c4bb872
NicoelRepo <nicolasrepollo2000@hotmail.com>
2025-10-24 15:45:46 -0300

    Se cambia el default environment a local

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 3000,
-  nodeEnv: process.env.NODE_ENV || 'development',
+  nodeEnv: process.env.NODE_ENV || 'local',
   database: {
     type: 'postgres' as const,
     host: process.env.DB_HOST || 'localhost',
